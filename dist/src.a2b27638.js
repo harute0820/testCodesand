faef286c8e717ead91b80d2901db0606225e1eca
harute0820 <harute.0820@gmail.com>
2022-11-19 00:53:28 +0000

    再々修正

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -202,7 +202,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44233" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46071" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
